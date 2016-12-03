2983393b7ae61f4a545c8393bb16e0d60c668d09
oliverlai55 <oliverlai55@gmail.com>
2016-12-03 13:55:46 -0500

    Set up axis url

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const FETCH_CALENDAR_DATA = 'FETCH_CALENDAR_DATA';
 
-const CALENDAR_URL = 'localhost:3000';
+const CALENDAR_URL = 'localhost:3000/eventHR';
 
 export function fetchCalendarData() {
   const request = axios.get({CALENDAR_URL});
